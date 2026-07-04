67d21690d5bd29ae3026d54b30c31ca54b30f525
CHETAN-197 <indiangaming919@gmail.com>
2026-07-04 13:26:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-function show(){w.value=localStorage.weight||'';wa.value=localStorage.waist||'';out.innerText='Weight: '+w.value+' kg | Waist: '+wa.value+' in';} function saveData(){localStorage.weight=w.value;localStorage.waist=wa.value;show();} show();
+const w = document.getElementById("weight");
+const wa = document.getElementById("waist");
+const out = document.getElementById("output");
+
+function saveData() {
+  localStorage.setItem("weight", w.value);
+  localStorage.setItem("waist", wa.value);
+  showData();
+}
+
+function showData() {
+  w.value = localStorage.getItem("weight") || "";
+  wa.value = localStorage.getItem("waist") || "";
+  out.innerText =
+    "Weight: " + (w.value || "-") + " kg | Waist: " + (wa.value || "-") + " in";
+}
+
+showData();
